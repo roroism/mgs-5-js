fcee4822a825757c03bf8638434c728897d2b687
roroism <miroiro0303@gmail.com>
2022-07-27 17:48:52 +0900

    feat: created an event for the input tag

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const ajax = new XMLHttpRequest();
 const NEWS_URL =
-  'https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=headline:("korea")&page=1&sort=newest&api-key=atuLPNUKKa8AhV1aMr5zs2c1lNymmGsr';
+  'https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=headline:("@searchkeyword")&page=1&sort=newest&api-key=atuLPNUKKa8AhV1aMr5zs2c1lNymmGsr';
+
+async function getData(url) {
+  const response = await fetch(url);
+  return await response.json();
+}
+
+// function getData(url) {
+//   fetch(url).then(response =>response.json())
+// }
 
 ajax.open("GET", NEWS_URL, false);
 ajax.send();
 
-const newsList = JSON.parse(ajax.response);
+// let newsList;
 
 const mainPageTemplate = `
   <div class="wrapper">
@@ -17,32 +26,43 @@ const mainPageTemplate = `
     </div>
     <main>
     </main>
-    <footer>
-    </footer>
   </div>
 `;
 
 // 첫 화면 렌더링
 init();
-// 뉴스 리스트 가져오기
-printNewsList();
 
-console.log(newsList);
-console.log("response.docs", newsList.response.docs);
+// console.log(newsList);
+// console.log("response.docs", newsList.response.docs);
 
 // document.getElementById("root").appendChild(ul);
 
 function init() {
   document.getElementById("root").innerHTML = mainPageTemplate;
+  const searchInputEl = document.getElementById("inputSearch");
+
+  // 뉴스 리스트 가져오기
+  // printNewsList(NEWS_URL.replace("@searchkeyword", "korea"));
+
+  searchInputEl.addEventListener("keyup", () => {
+    console.log("input keyup");
+    setTimeout(() => {
+      console.log(searchInputEl.value);
+    }, 500);
+  });
 }
 
-function printNewsList() {
+function printNewsList(url) {
   const ul = document.createElement("ul");
+
+  const newsList = getData(url);
+  // console.log(newsList);
   newsList.response.docs.forEach((news) => {
     const li = document.createElement("li");
     li.innerText = news.headline.main;
     ul.appendChild(li);
     // console.log("foreach");
   });
-  document.querySelector("main").appendChild();
+  console.log(ul);
+  document.querySelector("main").appendChild(ul);
 }
