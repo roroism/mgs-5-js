a2b40c1f6157fc16c85922acebde5c4097354ab7
roroism <miroiro0303@gmail.com>
2022-07-31 22:09:24 +0900

    feat: create 'unclip this'button

--- app.js
+++ app.js
@@ -49,184 +49,203 @@ const mainPageTemplate = `
   </div>
 `;
 
-const mainPageListTemplate = `
-  
-`;
-
 // input 태그의 Element입니다.
 let searchInputEl;
 
 // 첫 화면을 렌더링 하고, event를 등록합니다.
-init();
+// init();
+
+// function init() {
+document.getElementById("root").innerHTML = mainPageTemplate;
+searchInputEl = document.getElementById("inputSearch");
 
 const cliplistWrapperEl = document.querySelector("main.cliplist_wrapper");
 
-function init() {
-  document.getElementById("root").innerHTML = mainPageTemplate;
-  searchInputEl = document.getElementById("inputSearch");
+let setTimeoutId;
 
-  let setTimeoutId;
+// 검색창의 이벤트입니다.
+searchInputEl.addEventListener("keyup", (e) => {
+  if (
+    e.key === "Tab" ||
+    e.key === "CapsLock" ||
+    e.key === "Shift" ||
+    e.key === "Control" ||
+    e.key === "HanjaMode" ||
+    e.key === "HangulMode" ||
+    e.key === "ContextMenu" ||
+    e.key === "Meta" ||
+    e.key === "Alt" ||
+    e.key === "Pause" ||
+    e.key === "Caps Lock" ||
+    e.key === "Escape" ||
+    e.key === "Page Up" ||
+    e.key === "Page Down" ||
+    e.key === "End" ||
+    e.key === "Home" ||
+    e.key === "ArrowLeft" ||
+    e.key === "ArrowUp" ||
+    e.key === "ArrowRight" ||
+    e.key === "ArrowDown" ||
+    e.key === "Insert" ||
+    e.key === "Delete" ||
+    e.key === "Windows" ||
+    e.key === "Right click" ||
+    e.key === "NumLock" ||
+    e.key === "ScrollLock" ||
+    e.key === "PrintScreen" ||
+    e.key === "PageDown" ||
+    e.key === "PageUp" ||
+    e.key === "F1" ||
+    e.key === "F2" ||
+    e.key === "F3" ||
+    e.key === "F4" ||
+    e.key === "F5" ||
+    e.key === "F6" ||
+    e.key === "F7" ||
+    e.key === "F8" ||
+    e.key === "F9" ||
+    e.key === "F10" ||
+    e.key === "F11" ||
+    e.key === "F12"
+  ) {
+    return;
+  }
 
-  // 검색창의 이벤트입니다.
-  searchInputEl.addEventListener("keyup", (e) => {
-    if (
-      e.key === "Tab" ||
-      e.key === "CapsLock" ||
-      e.key === "Shift" ||
-      e.key === "Control" ||
-      e.key === "HanjaMode" ||
-      e.key === "HangulMode" ||
-      e.key === "ContextMenu" ||
-      e.key === "Meta" ||
-      e.key === "Alt" ||
-      e.key === "Pause" ||
-      e.key === "Caps Lock" ||
-      e.key === "Escape" ||
-      e.key === "Page Up" ||
-      e.key === "Page Down" ||
-      e.key === "End" ||
-      e.key === "Home" ||
-      e.key === "ArrowLeft" ||
-      e.key === "ArrowUp" ||
-      e.key === "ArrowRight" ||
-      e.key === "ArrowDown" ||
-      e.key === "Insert" ||
-      e.key === "Delete" ||
-      e.key === "Windows" ||
-      e.key === "Right click" ||
-      e.key === "NumLock" ||
-      e.key === "ScrollLock" ||
-      e.key === "PrintScreen" ||
-      e.key === "PageDown" ||
-      e.key === "PageUp" ||
-      e.key === "F1" ||
-      e.key === "F2" ||
-      e.key === "F3" ||
-      e.key === "F4" ||
-      e.key === "F5" ||
-      e.key === "F6" ||
-      e.key === "F7" ||
-      e.key === "F8" ||
-      e.key === "F9" ||
-      e.key === "F10" ||
-      e.key === "F11" ||
-      e.key === "F12"
-    ) {
-      return;
-    }
+  if (setTimeoutId) {
+    clearTimeout(setTimeoutId);
+    setTimeoutId = null;
+  }
 
-    if (setTimeoutId) {
-      clearTimeout(setTimeoutId);
-      setTimeoutId = null;
-    }
+  // 0.5초 후 검색을 진행합니다.
+  const searchkeyword = searchInputEl.value.trim();
+  if (searchkeyword) {
+    setTimeoutId = setTimeout(() => {
+      console.log("0.5초후 printNewsList 실행");
+      store.page = 1;
+      printNewsList(
+        NEWS_URL.replace("@searchkeyword", searchkeyword),
+        NEW_SEARCH
+      );
+      // 검색에 성공했으므로 검색어를 배열에 저장하고 출력하는 함수로 보냅니다.
+      saveSearchWordtoHistory(searchkeyword);
 
-    // 0.5초 후 검색을 진행합니다.
+      // 수행된 검색에 대한 결과 정보 출력합니다.
+      searchResult();
+    }, 500);
+  }
+});
+
+const searchHistoryEl = document.querySelector(".search_history_wrapper");
+//검색창에 focus를 하면 history가 나오게하는 이벤트입니다.
+searchInputEl.addEventListener("focus", () => {
+  searchHistoryEl.classList.add("on");
+});
+searchInputEl.addEventListener("focusout", () => {
+  searchHistoryEl.classList.remove("on");
+});
+
+// 무한 스크롤을 위한 스크롤 이벤트입니다. 스크롤의 위치를 감지합니다.
+window.addEventListener("scroll", () => {
+  // clip된 뉴스들이 보이는 화면에서는 동작하지 않게 합니다.
+  if (cliplistWrapperEl.classList.contains("on")) return;
+
+  let scrollLocation = document.documentElement.scrollTop; // 현재 스크롤바 위치
+  let windowHeight = window.innerHeight; // 스크린 창
+  let fullHeight = document.body.scrollHeight; //  margin 값은 포함 x
+
+  // 스크롤이 맨 마지막에 도달하면..
+  if (scrollLocation + windowHeight >= fullHeight) {
     const searchkeyword = searchInputEl.value.trim();
+    console.log("searchkeyword", searchkeyword);
+    // 검색어가 null이 아니라면
     if (searchkeyword) {
-      setTimeoutId = setTimeout(() => {
-        console.log("0.5초후 printNewsList 실행");
-        store.page = 1;
-        printNewsList(
-          NEWS_URL.replace("@searchkeyword", searchkeyword),
-          NEW_SEARCH
-        );
-        // 검색에 성공했으므로 검색어를 배열에 저장하고 출력하는 함수로 보냅니다.
-        saveSearchWordtoHistory(searchkeyword);
-
-        // 수행된 검색에 대한 결과 정보 출력합니다.
-        searchResult();
-      }, 500);
+      ++store.page;
+      printNewsList(
+        NEWS_URL.replace("@searchkeyword", searchkeyword),
+        ADD_SEARCH
+      );
     }
-  });
+    document.querySelector("body").style.backgroundColor = "red";
+  } else {
+    document.querySelector("body").style.backgroundColor = "blue";
+  }
+});
 
-  const searchHistoryEl = document.querySelector(".search_history_wrapper");
-  //검색창에 focus를 하면 history가 나오게하는 이벤트입니다.
-  searchInputEl.addEventListener("focus", () => {
-    searchHistoryEl.classList.add("on");
-  });
-  searchInputEl.addEventListener("focusout", () => {
-    searchHistoryEl.classList.remove("on");
-  });
+// 버튼을 클릭하면 clip화면과 검색화면을 switching 해주는 이벤트입니다.
+const searchWrapperEl = document.querySelector(".search_wrapper");
+const newslistWrapperEl = document.querySelector("main.newslist_wrapper");
 
-  // 무한 스크롤을 위한 스크롤 이벤트입니다. 스크롤의 위치를 감지합니다.
-  window.addEventListener("scroll", () => {
-    let scrollLocation = document.documentElement.scrollTop; // 현재 스크롤바 위치
-    let windowHeight = window.innerHeight; // 스크린 창
-    let fullHeight = document.body.scrollHeight; //  margin 값은 포함 x
-
-    // 스크롤이 맨 마지막에 도달하면..
-    if (scrollLocation + windowHeight >= fullHeight) {
-      const searchkeyword = searchInputEl.value.trim();
-      console.log("searchkeyword", searchkeyword);
-      // 검색어가 null이 아니라면
-      if (searchkeyword) {
-        ++store.page;
-        printNewsList(
-          NEWS_URL.replace("@searchkeyword", searchkeyword),
-          ADD_SEARCH
-        );
-      }
-      document.querySelector("body").style.backgroundColor = "red";
+document
+  .querySelector(".clipped_news_wrapper > button")
+  .addEventListener("click", () => {
+    if (searchWrapperEl.classList.contains("on")) {
+      searchWrapperEl.classList.remove("on");
+      newslistWrapperEl.classList.remove("on");
+      cliplistWrapperEl.classList.add("on");
     } else {
-      document.querySelector("body").style.backgroundColor = "blue";
+      cliplistWrapperEl.classList.remove("on");
+      searchWrapperEl.classList.add("on");
+      newslistWrapperEl.classList.add("on");
     }
   });
 
-  // 버튼을 클릭하면 clip화면과 검색화면을 switching 해주는 이벤트입니다.
-  const searchWrapperEl = document.querySelector(".search_wrapper");
-  const newslistWrapperEl = document.querySelector("main.newslist_wrapper");
+// 렌더 된 검색목록에 있는 clip this / unclip this 버튼 이벤트입니다.
+const newslistMainEl = document.querySelector("main.newslist_wrapper");
+newslistMainEl.addEventListener("click", (e) => {
+  // click 한 요소가 clip this 버튼인지 확인합니다.
+  if (e.target.classList.contains("clip_button")) {
+    // 이미 clip된 요소인지 아닌지 검사하고,
+    // clip된 것이라면 clipList 배열에서 삭제합니다.
+    if (e.target.parentElement.classList.contains("clipped")) {
+      const index = store.clipList.findIndex((clippedNews) => {
+        return clippedNews._id === e.target.parentElement.dataset._id;
+      });
+      store.clipList.splice(index, 1);
 
-  document
-    .querySelector(".clipped_news_wrapper > button")
-    .addEventListener("click", () => {
-      if (searchWrapperEl.classList.contains("on")) {
-        searchWrapperEl.classList.remove("on");
-        newslistWrapperEl.classList.remove("on");
-        cliplistWrapperEl.classList.add("on");
-      } else {
-        cliplistWrapperEl.classList.remove("on");
-        searchWrapperEl.classList.add("on");
-        newslistWrapperEl.classList.add("on");
-      }
+      e.target.parentElement.classList.remove("clipped");
+      e.target.innerText = "Clip this";
+      // console.log(store.clipList);
+    } else {
+      // clip안 된 것이라면,
+      // newsList 배열에서 해당 item을 찾아서 clipList에 넣습니다.
+      store.clipList.push(
+        store.newsList.find(
+          (item) => item._id === e.target.parentElement.dataset._id
+        )
+      );
+      e.target.parentElement.classList.add("clipped");
+      e.target.innerText = "Unclip this";
+      // console.log(store.clipList);
+    }
+    // 'clip된 뉴스만 보기' 화면에 재반영합니다.
+    printClipList();
+  }
+});
+
+// 렌더 된 clip목록에 있는 unclip this 버튼 이벤트입니다.
+
+cliplistWrapperEl.addEventListener("click", (e) => {
+  if (e.target.classList.contains("clip_button")) {
+    // cliplist에서 해당 item을 삭제하고, 렌더된 해당 엘리먼트도 삭제합니다.
+    const index = store.clipList.findIndex((clippedNews) => {
+      return clippedNews._id === e.target.parentElement.dataset._id;
     });
+    store.clipList.splice(index, 1);
+    e.target.parentElement.remove();
 
-  // 렌더 된 검색목록에 있는 clip this 혹은 unclip this 버튼 이벤트입니다.
-  const newslistMainEl = document.querySelector("main.newslist_wrapper");
-  newslistMainEl.addEventListener("click", (e) => {
-    // click 한 요소가 clip this 버튼인지 확인합니다.
-    if (e.target.classList.contains("clip_button")) {
-      // 이미 clip된 요소인지 아닌지 검사하고,
-      // clip된 것이라면 clipList 배열에서 삭제합니다.
-      if (e.target.parentElement.classList.contains("clipped")) {
-        const index = store.clipList.findIndex((clippedNews) => {
-          return clippedNews._id === e.target.parentElement.dataset._id;
-        });
-        store.clipList.splice(index, 1);
-
-        e.target.parentElement.classList.remove("clipped");
-        e.target.innerText = "Clip this";
-        // console.log(store.clipList);
-      } else {
-        // clip안 된 것이라면,
-        // newsList 배열에서 해당 item을 찾아서 clipList에 넣습니다.
-        store.clipList.push(
-          store.newsList.find(
-            (item) => item._id === e.target.parentElement.dataset._id
-          )
-        );
-        e.target.parentElement.classList.add("clipped");
-        e.target.innerText = "Unclip this";
-        // console.log(store.clipList);
-      }
-      // 'clip된 뉴스만 보기' 화면에 재반영합니다.
-      printClipList();
+    // 레더 된 검색된 기사목록에서도 해당 기사가 있는지 찾아서 있다면,
+    // unclip으로 된 버튼을 clip으로 바꾸고 class="clipped"도 삭제합니다.
+    const willChangeEl = document.querySelector(
+      `main.newslist_wrapper > ul > li[data-_id='${e.target.parentElement.dataset._id}']`
+    );
+    if (willChangeEl) {
+      willChangeEl.classList.remove("clipped");
+      willChangeEl.querySelector(".clip_button").innerText = "Clip this";
+      console.log("진입성공");
     }
-  });
-
-  // 렌더 된 clip목록에 있는 unclip this 버튼 이벤트입니다.
-  const cliplistMainEl = document.querySelector("main.cliplist_wrapper");
-  cliplistMainEl.addEventListener(() => {});
-}
+  }
+});
+// }
 
 // json데이터를 가져와서 화면에 뉴스리스트를 출력합니다.
 async function printNewsList(url, searchType) {
@@ -273,8 +292,10 @@ async function printNewsList(url, searchType) {
 
     // 이미 clip 된 뉴스인지 아닌지 검사합니다.
     if (
-      store.clipList.find((clipedNews) => {
-        return store.newList[i]._id === clipedNews._id;
+      store.clipList.find((clippedNews) => {
+        // console.log("store.newList[i]._id", store.newsList[i]._id);
+        // console.log("clippedNews._id", clippedNews._id);
+        return store.newsList[i]._id === clippedNews._id;
       })
     ) {
       clipBtn.innerText = "Unclip this";
